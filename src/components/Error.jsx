acc03258031586747b6235b8a80a9205d7b9f59c
manikangkandas <manikangkan.das@outlook.com>
2022-06-06 01:36:32 +0530

    adding dist

--- src/components/Error.jsx
+++
@@ -1,9 +0,0 @@
-const Error = () => {
-  return (
-    <div className='h-full grid place-content-center'>
-      <h1 className="text-xl font-semibold text-slate-100">This page is under construction👷🏻‍♀️</h1>
-    </div>
-  )
-}
-
-export default Error
